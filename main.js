0fa3c62846e00b605980f84c4ad396ead6709292
Vincent Brouwer <kipsensatie@gmail.com>
2017-09-30 13:24:40 +0200

    Remove request dep

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
-const request = require('request');
 const cors_anywhere = require('cors-anywhere');
 const app = express();
 const port = process.env.PORT || 3000;
